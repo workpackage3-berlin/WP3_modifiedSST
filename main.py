476286ad1faf920d4c8952364145be7e2437ff2a
workpackage3-berlin <161450935+workpackage3-berlin@users.noreply.github.com>
2024-02-27 13:21:18 +0100

    updated path from main script

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import subprocess
-import sys
-import os
 
 def run_script(script_path, conda_env):
-    conda_path = r'C:\\Users\\Juliette\\anaconda3\\Scripts\\conda.exe'  # Replace with the actual path
+    conda_path = r'C:\\Users\\lucia\\anaconda3\\Scripts\\conda.exe'  # Replace with the actual path
     cmd = [conda_path, 'run', '--no-capture-output', '-n', conda_env, 'python', script_path]
     subprocess.Popen(cmd, shell=True)
 
@@ -14,4 +12,4 @@ external_device_env = "tmsi"
 
 # Run scripts in their respective environments
 run_script('scripts\\modified_stop_signal_task_lsl.py', behavioral_task_env)
-run_script('scripts\\stream_lsl_eeg_jv.py', external_device_env)
+run_script('scripts\\stream_lsl_eeg.py', external_device_env)
